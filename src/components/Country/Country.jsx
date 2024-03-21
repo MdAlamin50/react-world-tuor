e5ca5f1d3aa557af9ae17025d571e189b59b394f
Md Alamin50 <mdalaminsk0101@gmail.com>
2024-03-21 20:36:18 +0600

    added the color select

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
+import "./country.css";
+const Country = ({ country , handleVisitedCountry}) => {
+  const { name, flags, area, population, cca3 } = country;
 
-import'./country.css';
-const Country = ({country}) => {
-    const {name, flags}= country; 
-    return (
-        <div className='country'>
-            <h3>Name:{name?.common}</h3>
-            <img src={flags.png} alt="" />
-            
-        </div>
-    );
+  const [visited, setVisited] = useState(false);
+
+  const handleVisited = () => {
+    setVisited(!visited);
+  };
+
+  return (
+    <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
+      <h3 style={{color: visited ? 'purple': 'white'}}>Name:{name.common}</h3>
+      <img src={flags.png} alt="" />
+      <p>area:{area}</p>
+      <p>Population:{population}</p>
+      <p>
+        <small>code:{cca3}</small>
+      </p>
+      <button>FMark visited</button><br /><br />
+      <button onClick={handleVisited}>{visited  ? 'Visited' : 'Going'}</button>
+      {visited ? "I have visited this country." : "I wont to visited"}
+    </div>
+  );
 };
 
 export default Country;
